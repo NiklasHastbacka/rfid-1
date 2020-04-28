18c869317801b4738fc659a74c2da10f8aa951b9
oscarilvoranta <oscar.ilvoranta@gmail.com>
2020-04-28 16:59:29 +0300

    a

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const bodyParser = require('body-parser');
+var app = require('express')();
+var cors = require('cors')
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-const sqlite3 = require('sqlite3').verbose()
-const DBSOURCE = "data.db"
-let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if(err){
-        console.log(err.message)
-        throw err
-    }
-    else{
-        console.log('Connected to SQlite databaswe.')
-    }
-})
+app.use(cors());
+app.set('socketio', io);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.get('/f', (req, res) => res.send('hello World!'))
 
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.get("/rfid/:rfid/:milli", (req, res) => { //http://localhost:3000/usermedia/2
+    res.type('json')
+    let rfid = req.params.rfid;
+    let milli = req.params.milli;
+    var io = req.app.get('socketio');
+    console.log(rfid + " " + milli)
+    io.emit('rfid', {rfid : rfid, milli: milli});
+    res.send({rfid : rfid, milli: milli})
+  })
 
-app.get("/tag/:tag/:time", function(req, res){
-    db.run(`INSERT INTO tags (tag, time) VALUES('${req.params.tag}', '${req.params.time}')`, function(err){
-        if(err){
-            return console.log(err.message);
-        }
-        
-    })
-})
-
-app.listen(port, () => console.log(`Listening on ${port}`))
+http.listen(4444, () => {
+  console.log('listening on *:4444');
+});
